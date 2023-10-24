e2eb5bacbb3d913c91ca7fbbfec19578c09c6c28
swordmaster2005 <soulmaster9794@gmail.com>
2023-10-24 21:05:47 +0530

    xcvdsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import MoreInfo from './pages/MoreInfo'
 import Home from './pages/Home'
 
 
 const App = () => {
- 
+  const routes=createBrowserRouter([
+    {
+      path:"/",
+      element:<Home/>
+    },
+    {
+      path:'social',
+      element:<MoreInfo/>
+    }
+  ])
 
   return (
-    <Home/>
+    <RouterProvider router={routes}/>
   )
 }
 
